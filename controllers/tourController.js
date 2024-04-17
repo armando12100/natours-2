6444302f443e8d6a5f8362253d12facbf295ae7e
Armando Sanchez <asan5900@gmail.com>
2024-04-17 14:52:16 -0700

    tourModel added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,20 +1,4 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-    console.log(`Tour id is: ${val} `)
-    
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-    next();
-};
+const Tour = require('./../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
     if (!req.body.name || !req.body.price) {
@@ -24,7 +8,7 @@ exports.checkBody = (req, res, next) => {
         });
     }
     next();
-}
+};
 
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
@@ -32,10 +16,10 @@ exports.getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success',
         requestedAT: req.requestTime,
-        results: tours.length,
-        data: {
-            tours
-        }
+        // results: tours.length,
+        // data: {
+        //     tours
+        // }
     });
 };
 
@@ -44,30 +28,23 @@ exports.getTour = (req, res) => {
 
     const id = req.params.id * 1;
 
-    const tour = tours.find(el => el.id === id);
+    // const tour = tours.find(el => el.id === id);
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
-    });
+    // res.status(200).json({
+    //     status: 'success',
+    //     data: {
+    //         tour
+    //     }
+    // });
 }
 
 exports.createTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId}, req.body);
-
-    tours.push(newTour);
-
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tours: newTour
-            }
-        })
-    });
+    res.status(201).json({
+        status: 'success',
+        // data: {
+        //     tours: newTour
+        // }
+    })
 };
 
 exports.updateTour = (req, res) => {
--- server.js
+++ server.js
@@ -8,24 +8,6 @@ const DB = process.env.DATABASE
 
 mongoose.connect(DB).then(() => console.log("DB Connection Successful!"));
 
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true
-    },
-    rating: {
-        type: Number,
-        default: 4.5
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price']
-    }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
 // Start Server //
 const port = process.env.PORT || 3000
 
