216ee52e4e4990f3e9ad1f0a1fe14a7ea1727cd2
Armando Sanchez <asan5900@gmail.com>
2024-04-17 10:42:59 -0700

    tour model and schema added

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
 const app = require('./app');
 
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE
+
+mongoose.connect(DB).then(() => console.log("DB Connection Successful!"));
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 // Start Server //
 const port = process.env.PORT || 3000
